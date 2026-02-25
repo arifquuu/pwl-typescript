24b906a7e96226b06c8632a954974ecd246ab001
arifquuu <arifqukeren@gmail.com>
2026-02-25 04:44:20 +0000

    .

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// src/index.ts
+
+function greet(name: string, umur: number): string {
+    return `Selamat datang `+name+` di PWL, umur `+name+` adalah;
+}
+
+const mahasiswa = "Shalwa";
+const umur: number = 19;
+console.log(greet(mahasiswa,umur));
