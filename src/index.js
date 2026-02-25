4f6b64ff5199904d3347e0520e589150fbdf0875
arifquuu <arifqukeren@gmail.com>
2026-02-25 04:45:11 +0000

    lol

---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// src/index.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Selamat datang ` + name + ` di PWL`;
+}
+console.log(greet('Arifqu'));
+//# sourceMappingURL=index.js.map
